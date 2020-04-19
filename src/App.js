db16da11a12376a9d7649806785021a698deefef
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-04-19 18:55:55 +0900

    add: 公式チュートリアルの通りに実装

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import './App.css';
 import GlitchClip from 'react-glitch-effect/core/Clip'
+import dayjs from 'dayjs'
+
+dayjs.locale('ja')
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      ymd : dayjs().format('YYYY / MM / DD'),
+      hms : dayjs().format('HH:mm:ss')
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      ()=>this.tick(),
+      100
+    )
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timerID)
+  }
+
+  tick () {
+    this.setState({
+      ymd : dayjs().format('YYYY / MM / DD'),
+      hms : dayjs().format('HH:mm:ss')
+    })
+  }
+
+  render() {
+    return (
+    <div>
+      <p>{this.state.ymd}</p>
+      <p>{this.state.hms}</p>
+    </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <GlitchClip>
-        <p>
-          this is a simple clock app.
-        </p>
+        <Clock />
         </GlitchClip>
       </header>
     </div>
