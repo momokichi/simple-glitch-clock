0547b53310d4636fc193a02ae1afb43f116b3861
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-04-19 17:34:39 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GlitchClip from 'react-glitch-effect/core/Clip'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <GlitchClip>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          this is a simple clock app.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </GlitchClip>
       </header>
     </div>
   );
